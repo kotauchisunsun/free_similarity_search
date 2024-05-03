d396c2d44e248cdcfcee15e96f0c6922f03371ff
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2024-05-03 16:12:56 +0900

    modify

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-apt install clang libomp-dev build-essential
+sudo apt-get update
+sudo apt-get install -y clang libomp-dev build-essential
 curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 source "$HOME/.rye/env"
 rye sync -f
